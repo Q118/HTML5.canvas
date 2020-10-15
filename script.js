ecbcd045a2885a8e5e072012e1765680ca72cea2
Q118 <shelbyfish91@gmail.com>
2020-10-15 12:20:31 -0700

    appropriating isDrawing method

--- script.js
+++ script.js
@@ -19,3 +19,6 @@ function draw(e) {
 }
 
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+
