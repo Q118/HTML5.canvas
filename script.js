109d969c95cbf01dc8c79a48e0f07c44e118db5c
Q118 <shelbyfish91@gmail.com>
2020-10-15 12:19:18 -0700

    initializing event listeners for mousedown

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
 let isDrawing = false;
+let lastX = 0;
+let lastY =0;
+
+//* function called whenever mouse is moved on canvas
+function draw(e) {
+    if(!isDrawing) return; //stop the function from running when user is not moused down
+    console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
