0de70dcd48dcc596df6159b37922d3951530e8d4
Q118 <shelbyfish91@gmail.com>
2020-10-15 12:15:14 -0700

    stroke style and variable declarations

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
